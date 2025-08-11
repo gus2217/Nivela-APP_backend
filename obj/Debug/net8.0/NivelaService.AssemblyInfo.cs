965ace47b0f25b0df50b7084186c9432f85949be
Karol <kiptoocarroll@gmail.com>
2025-08-11 07:49:01 +0300

    x

--- obj/Debug/net8.0/NivelaService.AssemblyInfo.cs
+++ obj/Debug/net8.0/NivelaService.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("NivelaService")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3d4efe43ced1c9328a8e449487f8e0e4ac1590d8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8201b82bde5096c564a9a4cc9b776265fbfc8c7e")]
 [assembly: System.Reflection.AssemblyProductAttribute("NivelaService")]
 [assembly: System.Reflection.AssemblyTitleAttribute("NivelaService")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
